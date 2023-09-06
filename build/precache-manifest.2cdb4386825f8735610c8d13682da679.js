67052735b3886273bc1b402b3344c2db78b1fb05
davc1980 <davc1980@gmail.com>
2023-09-06 08:45:05 -0700

    site builded

---
+++ build/precache-manifest.2cdb4386825f8735610c8d13682da679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914ad92753064906c9c5e3aaec4748d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff89acd1a031f9aa4919",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "354679336dbfd276deb9",
+    "url": "/static/js/2.701c1f79.chunk.js"
+  },
+  {
+    "revision": "11a828666d33f8c65ca019a37139cfe9",
+    "url": "/static/js/2.701c1f79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff89acd1a031f9aa4919",
+    "url": "/static/js/main.914cfd6e.chunk.js"
+  },
+  {
+    "revision": "0d3620d7490d483db568",
+    "url": "/static/js/runtime-main.b5594142.js"
+  }
+]);
